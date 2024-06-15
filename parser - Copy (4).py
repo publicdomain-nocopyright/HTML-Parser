f8db7f8946357e542769867e45e6af3c4bcabb3a
Feldwor <BoQsc@users.noreply.github.com>
2024-06-15 19:18:15 +0300

    Create parser - Copy (4).py

---
+++ parser - Copy (4).py
@@ -0,0 +1,34 @@
+import parser_library
+
+# Tokenization is needed to easily select tokens and group, target them against other tokens.
+# Associate p start tag with p end tag. 
+
+
+def begintokenization():
+    global tokenization
+    tokenization = True
+
+def stoptokenization():
+    global tokenization
+    tokenization = False
+
+def htmltotokens(htmlstring, token = "", closingtag = False):
+    for letter in htmlstring:
+        if letter == '<': begintokenization(); continue
+        if letter == '>':  stoptokenization(); print(token, closingtag);   token = ""; 
+            
+        if tokenization: token += letter
+        if tokenization and letter == "/": closingtag = True
+
+
+
+
+
+       
+           
+           
+
+
+htmltotokens(parser_library.htmltostring("example.html"))
+print()
+print("Tokenization")
