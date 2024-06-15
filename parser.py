f9b574dc4cfdd4373c67fda907a862beaa2a0bce
Feldwor <BoQsc@users.noreply.github.com>
2024-06-15 16:43:52 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -4,21 +4,16 @@ import parser_library
 # Associate p start tag with p end tag. 
 def htmltotokens(htmlstring, token = "", starttokenization = False, closingtag = False):
     for letter in htmlstring:
-        if starttokenization and not letter == '>':
-            token += letter
-            if letter == "/":
-                closingtag = True
-
-        if starttokenization == False and not token == "":
-            print(token)
+        if starttokenization and not letter == '>': token += letter
+        if starttokenization and letter == "/": closingtag = True
+        if starttokenization == False and not token == "": print(token)
 
         if letter == '<': starttokenization = True
         if letter == '>': starttokenization = False
 
         if letter == '>':
             print(token)
-            if closingtag:
-                print("It's a closing tag!")
+            if closingtag: print("It's a closing tag!")
             token = ""
             closingtag = False
 
