2532e5d04d862fec9b6906bbcf5f2ebbd022e5eb
Feldwor <BoQsc@users.noreply.github.com>
2024-06-18 16:12:26 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -8,12 +8,13 @@ import parser_library
 def htmltotokens(htmlstring):
     tokens = []
     token = ""
-    attributes = {"is_closing_tag": False, "waffles": 5}
-    reset_attributes = attributes.copy() 
+    init_attributes = {"is_closing_tag": False, "waffles": 5} 
+    attributes = init_attributes.copy() 
+
     is_tokenizing = False
 
     for letter in htmlstring:
-        if letter == '<': is_tokenizing = True; attributes.update(reset_attributes); continue      
+        if letter == '<': is_tokenizing = True; attributes.update(init_attributes); continue      
         if letter == '>': is_tokenizing = False; tokens.append((token, attributes["is_closing_tag"])); token = ""; continue;
 
         if is_tokenizing:
