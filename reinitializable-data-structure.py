71affec5be3e6daabb98dc998ef14dc165d71548
Feldwor <BoQsc@users.noreply.github.com>
2024-06-18 14:06:50 +0300

    Update reinitializable-data-structure.py

--- reinitializable-data-structure.py
+++ reinitializable-data-structure.py
@@ -1,17 +1,15 @@
-def attributes_parsing():
+def html_attributes_parsing():
     attributes = {"is_closing_tag": False, "waffles": 5}
     init_attributes = attributes.copy() 
-    
-    def reset_attributes():
-        attributes.update(init_attributes)
+        
     
     print("Initial attributes:", attributes)
     
     attributes["waffles"] = 4
     print("Modified attributes:", attributes)
     
-    reset_attributes()
+    attributes.update(init_attributes)
     print("Reset attributes:", attributes)
 
 
-attributes_parsing()
+html_attributes_parsing()
