37dbb5b33e117b436b01d106aac927ed47a585ab
Feldwor <BoQsc@users.noreply.github.com>
2024-06-20 00:21:23 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -26,10 +26,8 @@ def htmltotokens(htmlstring : str):
         
 
         if tokenizing:
-            if letter is '>':
-                 tokenizing = not tokenizing; 
-            if token is None: 
-                token = "" 
+            if letter is '>': tokenizing = not tokenizing; 
+            if token is None: token = "" 
             if token is not None and letter is not '>':
                 token += letter
                 if letter == '/':
