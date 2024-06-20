c8a16ee839261954a5a7c0aa5c021f870b81bf40
Feldwor <BoQsc@users.noreply.github.com>
2024-06-20 17:59:30 +0300

    update

--- parser.py
+++ parser.py
@@ -10,9 +10,8 @@ import parser_library
 
 
 def htmltotokens(htmlstring : str = None):
-    token  : str = None
-    tokens : list[str] = []
-    letter : str = None
+
+    default_attributes : dict[str, any] = {"is_closing_tag": False, "test": True} 
 
 
     # Tokenization definition, Capture tokens
@@ -26,69 +25,38 @@ def htmltotokens(htmlstring : str = None):
     }
 
     def start_tokenization():
+        nonlocal tokenization
         tokenization['state'] = 'running'
         tokenization['tokenizing'] = True
         tokenization['token'] = None #reset_token
         tokenization['attributes'] = default_attributes.copy() #reset_attributes
 
     def stop_tokenization():
+        nonlocal tokenization
         tokenization['state'] = 'stopped'
         tokenization['tokenizing'] = False
-        tokenization['tokens'].append((tokenization[token], tokenization[attributes])); 
+        if 'tokens' not in tokenization:
+            tokenization['tokens'] = []
+
         
-    for letter in htmlstring:
-        if letter is '<':
-            start_tokenization()
-
-        if tokenization['tokenizing']: 
-            tokenization['token'] += letter
-
-        if letter is '>':
-            stop_tokenization()
-
-    default_attributes : dict[str, any] = {"is_closing_tag": False, "test": True} 
-    attributes : dict[str, any] = default_attributes.copy()
-
-    tokenizing : bool = False
-
-    # Tag Scanning, processing, tokenization, html tag-markers (<>) and tag-name (<somename>) capture
-    for letter in htmlstring:
-        if letter is '<': # Trigger tokenization
-            tokenizing = True 
-            token = None; 
-            attributes = default_attributes.copy();
-            continue
         
+    def tokenize():
+        nonlocal tokenization
+        for letter in htmlstring:
+            if letter == '<':
+                start_tokenization()
 
-        if tokenizing:
-            if token is None: token = "" 
-            if letter is '>': tokenizing = not tokenizing; 
+            if tokenization['tokenizing']: 
+                if tokenization['token'] is None:
+                    tokenization['token'] = ""
+                tokenization['token'] += letter
 
-            if token is not None and letter is not '>':
-                token += letter
-                if letter == '/':
-                    attributes["is_closing_tag"] = True
-            
-        else:
-            print(letter)
-
-        if not tokenizing and token is not None:
-            tokens.append((token, attributes)); 
-            tokenizing = False
-            token = None
-            continue
- 
-    return tokens
-
+            if letter == '>':
+                stop_tokenization()
+                tokenization['tokens'].append((tokenization["token"], tokenization["attributes"])); 
+    tokenize()
+    return tokenization["tokens"]
 tokens = htmltotokens(parser_library.htmltostring("example.html"))
 
-print("____________")
 print(tokens)
-for token in tokens:
-    if token[0] is None:
-        print("stopped here" + str(token))
-        input()
-    for letter in token[0]:
-        if letter == ' ':
-            break
-        print(letter, end="")
+    
