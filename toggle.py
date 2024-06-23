2f861699f44559aa6566c74c082ce651db8fdfee
Feldwor <BoQsc@users.noreply.github.com>
2024-06-23 21:03:02 +0300

    Update toggle.py

--- toggle.py
+++ toggle.py
@@ -1,4 +1,4 @@
-# Toggle must be aware of its value in the context instead of global variable. If a different function call, it should reset itself. It should be unique to each function.
+# Toggle must be aware of its value in the context instead of being global variable. If a different function call, it should reset itself. It should be unique to each function.
 
 def togglere():
     state = True
