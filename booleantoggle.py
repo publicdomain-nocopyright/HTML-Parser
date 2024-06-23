cc9d51611a98e452bf21030c868a36d064f2a747
Feldwor <BoQsc@users.noreply.github.com>
2024-06-23 19:53:32 +0300

    Update booleantoggle.py

--- booleantoggle.py
+++ booleantoggle.py
@@ -9,3 +9,7 @@ toggle = boolean_toggle()
 print(toggle)
 toggle = boolean_toggle()
 print(toggle)
+
+# Generator and yield method is better, allowing for intercepting
+# if two different functions use booleanstate variable
+# One can toggle the previous to false, leading to unintended problems.
