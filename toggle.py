343847d7c47bf4ecab69ca6d051f741d2f84a912
Feldwor <BoQsc@users.noreply.github.com>
2024-06-23 21:01:02 +0300

    Update toggle.py

--- toggle.py
+++ toggle.py
@@ -1,13 +1,13 @@
 # Toggle must be aware of its value in the context. If a different function call, it should reset itself. It should be unique to each function.
 
-def toggle():
+def togglere():
     state = True
     while True:
         yield state
         state = not state
 
 # Using the generator
-toggle_gen = toggle()
+toggle_gen = togglere()
 
 # First call to get initial state
 print(next(toggle_gen))  # Output: True
