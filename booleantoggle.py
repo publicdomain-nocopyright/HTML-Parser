1b55e7c98f8e42247de1d8de8f37c97e518dfb60
Feldwor <BoQsc@users.noreply.github.com>
2024-06-23 19:12:06 +0300

    Update booleantoggle.py

--- booleantoggle.py
+++ booleantoggle.py
@@ -2,7 +2,6 @@ def boolean_toggle():
 	global booleanstate
 	if 'booleanstate' not in globals():
 		booleanstate = False
-		print("test")
 	booleanstate = not booleanstate
 	return booleanstate
     
