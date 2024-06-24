fa6dbc623731d550f4610ebddb3f2ead87254053
Feldwor <BoQsc@users.noreply.github.com>
2024-06-24 11:27:03 +0300

    Update parser_tokenizer.py

--- parser_tokenizer.py
+++ parser_tokenizer.py
@@ -1,7 +1,7 @@
 
 from parser_library import *
 
-@execute
+
 def tokenizer():
     token = ''
     tokens = []
@@ -16,6 +16,43 @@ def tokenizer():
             
         #print(letter, end="")
         pass
+    print(tokens)
+
+
+@execute
+def tokenizer():
+    token = ''
+    tokens = []
+    htmltag = False
+    text = ''
+    start_text = False  
+
+    for letter in htmltostring("example.html"):
+
+        if letter == '<' and text:
+            if text:  
+                text += '</text>'
+                tokens.append(text)
+                text = ''
+            start_text = False  
+
+        if letter == '<': htmltag = True; continue
+        if letter == '>': htmltag = False; tokens.append(token);token = '' ; continue           
+        
+        if htmltag:
+            token += letter
+        if not htmltag:
+            if not start_text:  
+                text = '<text>'
+                start_text = True
+            
+        if not htmltag:  
+            text += letter
+
+    if text:  
+        text += '</text>'
+        tokens.append(text)
+    
     print(tokens)
 
 
